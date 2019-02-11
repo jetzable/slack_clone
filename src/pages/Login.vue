15ac4956688a7b71dd49bff9dd5a7e77bb9ee88a
jetzable <gjetzable@gmail.com>
2019-02-11 17:22:53 -0600

    Simple title

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,5 +1,10 @@
 <template>
-  <h2>Login</h2>
+  <div>
+    <div class="jumbotron bg-primary text-white text-center">
+      <h2 class="lead display-3">#SLACK#</h2>
+      <p>Instant communication!!!</p>
+    </div>
+  </div>
 </template>
 <script>
 export default {
